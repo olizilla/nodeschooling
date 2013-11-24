8cb65a222ff0ce7b019f153020348a7934db92bc
olizilla <oli@zilla.org.uk>
2013-11-24 13:50:58 +0000

    Adds `stream-adventure` answers

---
+++ stream-adventure-answers/01-beep-boop.js
@@ -0,0 +1,6 @@
+/* 
+Just to make sure everything is working, just write a `program.js` that outputs
+the string "beep boop" with a `console.log()`.
+*/
+
+console.log('beep boop')
---
+++ stream-adventure-answers/10-html-stream.js
@@ -0,0 +1,37 @@
+/*
+  #####################################################################
+  ##                       ~~  HTML STREAM  ~~                       ##
+  #####################################################################
+
+Your program will get some html written to stdin. Convert all the inner html to
+upper-case for elements with a class name of "loud".
+
+You can use `trumpet` and `through` to solve this adventure.
+
+With `trumpet` you can create a transform stream from a css selector:
+
+    var trumpet = require('trumpet');
+    var fs = require('fs');
+    var tr = trumpet();
+    fs.createReadStream('input.html').pipe(tr);
+    
+    var stream = tr.select('.beep').createStream();
+
+Now `stream` outputs all the inner html content at `'.beep'` and the data you
+write to `stream` will appear as the new inner html content.
+
+Make sure to `npm install trumpet through` in the directory where your solution
+file lives.
+*/
+var trumpet = require('trumpet')()
+var through = require('through')
+
+var upperCaser = through(function(buf){
+  this.queue((buf+'').toUpperCase())
+})
+
+var loud = trumpet.select('.loud').createStream()
+
+loud.pipe(upperCaser).pipe(loud)
+
+process.stdin.pipe(trumpet).pipe(process.stdout)
